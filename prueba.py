c146c72d244c3c86f9651b67fd79ba3250623bbe
CarlaVinasTemplado <81970830+CarlaVinasTemplado@users.noreply.github.com>
2022-09-05 11:11:41 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,2 +1,18 @@
-print("Hello world")
+import pandas as pd
 
+clients = {
+    "Waldon Astling": 1.83,
+    "Catherine MacTerlagh": 0.15,
+    "Gusty Wondraschek": 9.19,
+    "Lois Vaan": 1.28,
+    "Baird Eberts": 0.82,
+    "Amalia Flieg": 2.88,
+    "Leontine Wildbore": 9.44,
+    "Rikki Chasteney": 7.01,
+    "Augustine Papierz": 0.22,
+    "Maynord Lawrance": 0.33
+}
+
+df = pd.DataFrame([[key, clients[key]] for key in clients.keys()], columns=['Name', 'Amount'])
+
+print("El diccionario es el siguiente: \n", df)
