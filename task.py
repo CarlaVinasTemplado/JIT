c853848d97a3e569410b4732699a97c3941b4f90
CarlaVinasTemplado <81970830+CarlaVinasTemplado@users.noreply.github.com>
2022-09-06 12:01:18 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,14 +1,2 @@
-clients = {
-    "Waldon Astling": 1.83,
-    "Catherine MacTerlagh": 0.15,
-    "Gusty Wondraschek": 9.19,
-    "Lois Vaan": 1.28,
-    "Baird Eberts": 0.82,
-    "Amalia Flieg": 2.88,
-    "Leontine Wildbore": 9.44,
-    "Rikki Chasteney": 7.01,
-    "Augustine Papierz": 0.22,
-    "Maynord Lawrance": 0.33
-}
 
-print("El diccionario es el siguiente: \n", clients)
+print('holi')
