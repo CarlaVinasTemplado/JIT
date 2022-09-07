177ef25a2666d28c26c06b355948d61de6e11411
CarlaVinasTemplado <81970830+CarlaVinasTemplado@users.noreply.github.com>
2022-09-07 11:09:39 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,7 @@
 import sys
 
 lista= sys.argv[1] #primer elemento de la comanda desde el terminal
-print('lista: ', lista)
+print(lista)
+lista= lista.split("\n")
+
+print('lista de incidencias de la JIT anterior: ', lista)
