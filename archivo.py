3ce494364e9bf3cc1758ab1a5f8f21e04039eee5
CarlaVinasTemplado <81970830+CarlaVinasTemplado@users.noreply.github.com>
2022-09-08 10:13:47 +0200

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,10 +1,7 @@
 import sys
 import pandas as pd
 
-ruta= sys.argv[1] #primer elemento de la comanda desde el terminal
-print(ruta)
-
-df = pd.read_excel (r'{ruta}')
+df = pd.read_excel ('inc_file.xlsx')
 df
 
 
